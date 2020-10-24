5496139e1079b7c9a52b5383b8593581e15d0ee3
joshuajung01 <tojoshuajung@gmail.com>
2020-10-24 01:35:37 -0500

    initial flask

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, abort, reqparse, fields, marshal_with
+from wikipedia import wikipediaInfo
 import requests
 import json
 
@@ -9,6 +10,15 @@ api = Api(app)
 
 # to add routes do:
 # api.add_resource(route,'/urroute')
+@app.route("/")
+def home():
+    return('This is the API for our Hack TX Project')
+
+@app.route("/wikipedia")
+def pinterest():
+    query = request.args.get('q')
+    l = wikipediaInfo(query)
+    return jsonify(results = l)
 
 if __name__ == "__main__":
     app.run(debug=True)  # debug-True lets u see logs of what goes wrong
