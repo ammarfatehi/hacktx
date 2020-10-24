ef208a1c86087c67cb0a956ab6f42a55c0bc25a2
Ankith Lakshman <ankith.lakshman@berkeley.edu>
2020-10-24 10:09:31 -0500

    Bare Frontend App

---
+++ NutriApp/App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+            <View>
+              <Text>This is the Main Page</Text>
+            </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+  },
+});
+
+export default App;
