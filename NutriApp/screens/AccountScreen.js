65dc54bf7b1e38252d627be4a13986e7d7f72def
Ankith Lakshman <ankith.lakshman@berkeley.edu>
2020-10-24 12:11:24 -0500

    Navigation Working

---
+++ NutriApp/screens/AccountScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+    View,
+    Text,
+} from 'react-native';
+
+function AccountScreen(props) {
+
+    return(
+        <View>
+            <Text>
+                Account Screen
+            </Text>
+
+        </View>
+    )
+}
+
+
+export default AccountScreen;
---
+++ NutriApp/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+    View,
+    Text,
+} from 'react-native';
+
+function HomeScreen(props) {
+
+    return(
+        <View>
+            <Text>
+                Home Screen
+            </Text>
+
+        </View>
+    )
+}
+
+
+export default HomeScreen;
+
