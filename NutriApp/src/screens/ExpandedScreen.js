ecb5b6805031a9b151098f06cb465a346ba4716f
MihiranP <mihiranpandey360@gmail.com>
2020-10-25 08:35:46 -0500

    final product

---
+++ NutriApp/src/screens/ExpandedScreen.js
@@ -0,0 +1,64 @@
+import React, { Component, useState } from 'react';
+import AnimatedButton from '../components/AnimatedButton'
+import ItemCard from '../components/ItemCard'
+// import account from '../assets/close.png';
+
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    FlatList,
+    ActivityIndicator,
+    TouchableOpacity,
+    TouchableHighlight,
+    Button
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/Ionicons'
+import theme from '../utils/theme';
+function ExpandedScreen(props) {
+    const name = props.name; 
+    const [apireturn,setApireturn] = useState('');
+    const Http = new XMLHttpRequest();
+    const url='https://hacktx3.azurewebsites.net/wikipedia?q='+name;
+    console.log(url);
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {  
+        {setApireturn(JSON.parse(Http.responseText).results[0].text)}
+        
+    }
+    return(
+        <View>
+            <Text style={styles.title}>
+                {name} 
+            </Text>
+            
+            <Text style={styles.type}>
+                {apireturn}
+            </Text>
+        </View>
+    )
+}
+
+export default ExpandedScreen;
+
+const styles = StyleSheet.create({
+    title: {
+        fontFamily: theme.font.family.title,
+        fontWeight: 'bold',
+        fontSize: 24,
+        textAlign: 'center',
+        paddingTop: 15,
+    },
+    type: {
+        paddingTop: 0,
+        paddingBottom: 10,
+        margin: 20,
+        fontFamily: theme.font.family.title,
+        fontSize: 16,
+
+    },
+});
